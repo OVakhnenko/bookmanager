e7c0abc51246f4e28bd7b9dadaa0e256005aff26
Aleksey <oleksiy@vakhnenko.com>
2017-03-19 21:55:39 +0300

    ...

---
+++ src/main/java/com/vakhnenko/bookmanager/service/BookService.java
@@ -0,0 +1,17 @@
+package com.vakhnenko.bookmanager.service;
+
+import com.vakhnenko.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public void addBook(Book book);
+
+    public void updateBook(Book book);
+
+    public void removeBook(int id);
+
+    public Book getBookById(int id);
+
+    public List<Book> listBooks();
+}
